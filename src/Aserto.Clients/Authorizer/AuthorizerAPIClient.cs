a237b401d7ec84c2ceba90bedb7e14b0ff31ffec
carabasdaniel <dani@aserto.com>
2024-09-13 16:30:37 +0300

    Rebase and allow make authorizer policy instance label optional

--- src/Aserto.Clients/Authorizer/AuthorizerAPIClient.cs
+++ src/Aserto.Clients/Authorizer/AuthorizerAPIClient.cs
@@ -23,6 +23,7 @@ namespace Aserto.Clients.Authorizer
     using Microsoft.Extensions.Options;
     using static Aserto.Authorizer.V2.Authorizer;
     using System.Linq;
+    using System.Net;
 
     /// <summary>
     /// Client for Aserto Authorizer API.
@@ -94,6 +95,10 @@ namespace Aserto.Clients.Authorizer
                 logger.LogDebug($"Authentication type set to {isRequest.IdentityContext.Type}. Using identity ${isRequest.IdentityContext.Identity}");
             }
 
+            if (isRequest.PolicyInstance!=null && string.IsNullOrEmpty(isRequest.PolicyInstance.InstanceLabel)){
+                isRequest.PolicyInstance.InstanceLabel = isRequest.PolicyInstance.Name;
+            }
+
             logger.LogDebug($"Policy Context path resolved to: {isRequest.PolicyContext.Path}");
             logger.LogDebug($"Resource Context resolved to: {isRequest.ResourceContext}");
             var result = await authorizerClient.IsAsync(isRequest);
@@ -114,7 +119,11 @@ namespace Aserto.Clients.Authorizer
             {
                 throw new ArgumentNullException(nameof(listRequest));
             }
-            
+            if (listRequest.PolicyInstance!=null && string.IsNullOrEmpty(listRequest.PolicyInstance.InstanceLabel))
+            {
+                listRequest.PolicyInstance.InstanceLabel = listRequest.PolicyInstance.Name;
+            }
+
             logger.LogDebug($"List Policies - policies instance name: {listRequest.PolicyInstance.Name}");
             logger.LogDebug($"List Policies - policies instance label: {listRequest.PolicyInstance.InstanceLabel}");            
             var result = await authorizerClient.ListPoliciesAsync(listRequest);
@@ -128,7 +137,10 @@ namespace Aserto.Clients.Authorizer
             {
                 throw new ArgumentNullException(nameof(getRequest));
             }
-
+            if (getRequest.PolicyInstance!=null && string.IsNullOrEmpty(getRequest.PolicyInstance.InstanceLabel))
+            {
+                getRequest.PolicyInstance.InstanceLabel = getRequest.PolicyInstance.Name;
+            }
             logger.LogDebug($"Get Policy - policy ID: {getRequest.Id}");
             logger.LogDebug($"Get Policy - policy instance name: {getRequest.PolicyInstance.Name}");
             logger.LogDebug($"Get Policy - policy instance label: {getRequest.PolicyInstance.InstanceLabel}");
@@ -144,7 +156,10 @@ namespace Aserto.Clients.Authorizer
             {
                 throw new ArgumentNullException(nameof(queryRequest));
             }
-
+            if (queryRequest.PolicyInstance!=null && string.IsNullOrEmpty(queryRequest.PolicyInstance.InstanceLabel))
+            {
+                queryRequest.PolicyInstance.InstanceLabel = queryRequest.PolicyInstance.Name;
+            }
             logger.LogDebug($"Query Policy: {queryRequest.Query}");
             logger.LogDebug($"Identity Context resolved to: {queryRequest.IdentityContext}");
             var result = await authorizerClient.QueryAsync(queryRequest);
@@ -158,6 +173,10 @@ namespace Aserto.Clients.Authorizer
             if (compileRequest == null)
             {
                 throw new ArgumentNullException(nameof(compileRequest));
+            }
+            if (compileRequest.PolicyInstance!=null && string.IsNullOrEmpty(compileRequest.PolicyInstance.InstanceLabel))
+            {
+                compileRequest.PolicyInstance.InstanceLabel = compileRequest.PolicyInstance.Name;
             }
 
             logger.LogDebug($"Query Policy: {compileRequest.Query}");
@@ -173,6 +192,10 @@ namespace Aserto.Clients.Authorizer
             if (decisionTreeRequest == null)
             {
                 throw new ArgumentNullException(nameof(decisionTreeRequest));
+            }
+            if (decisionTreeRequest.PolicyInstance!=null && string.IsNullOrEmpty(decisionTreeRequest.PolicyInstance.InstanceLabel))
+            {
+                decisionTreeRequest.PolicyInstance.InstanceLabel = decisionTreeRequest.PolicyInstance.Name;
             }
 
             logger.LogDebug($"Identity Context resolved to: {decisionTreeRequest.IdentityContext}");
